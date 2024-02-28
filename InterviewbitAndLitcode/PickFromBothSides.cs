9cd241c528d54938a577fcb21a6e11f46e8eea13
Ernan <maisels@gmail.com>
2024-02-28 09:12:06 +0200

    PickFromBothSides

---
+++ InterviewbitAndLitcode/PickFromBothSides.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode
+{
+    public static class PickFromBothSides
+    {
+        public static int Solve(List<int> A, int B)
+        {
+            int n = A.Count;
+            int maxSum = 0;
+            int sum = 0;
+
+            for (int i = 0; i < B; i++)
+            {
+                sum += A[i];
+            }
+
+            maxSum = sum;
+
+            for (int i = 0; i < B; i++)
+            {
+                sum -= A[B - 1 - i];
+                sum += A[n - 1 - i];
+                maxSum = Math.Max(maxSum, sum);
+            }
+
+            return maxSum;
+        }
+    }
+}
--- InterviewbitAndLitcode/Program.cs
+++ InterviewbitAndLitcode/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             var pascalsTriangleRow = PascalsTriangle.GetKthRowOfPascalsTriangle(3);
+            var pickFromBothSides = PickFromBothSides.Solve(new List<int> { 5, -2, 3, 1, 2 }, 3);
         }
     }
 
