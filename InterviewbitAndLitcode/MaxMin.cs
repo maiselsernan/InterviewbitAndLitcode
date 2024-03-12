358087984239e061d286f0f27dfc6e8f14d47630
Ernan <maisels@gmail.com>
2024-03-12 13:11:42 +0200

    max min

---
+++ InterviewbitAndLitcode/MaxMin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode
+{
+    public static class MaxMin
+    {
+        //https://www.interviewbit.com/problems/max-min-05542f2f-69aa-4253-9cc7-84eb7bf739c4/
+        public static int Solve(List<int> A)
+        {
+            int max = A.Max();
+            int min = A.Min();
+
+            return max + min;
+        }
+
+    }
+}
