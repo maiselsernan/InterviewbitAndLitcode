47c8f1ec177389dad1cb03129145838e796f0500
Ernan <maisels@gmail.com>
2024-03-13 17:30:24 +0200

    RemoveElemen

---
+++ InterviewbitAndLitcode.Tests/RemoveElementTests.cs
@@ -0,0 +1,22 @@
+﻿namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass]
+    public class RemoveElementTests
+    {
+        [TestMethod]
+        public void RemoveElementMethodTest()
+        {
+            // Arrange
+            var removeElement = new RemoveElement();
+            var nums = new int[] { 3, 2, 2, 3 };
+            var val = 3;
+            var expected = 2;
+
+            // Act
+            var result = removeElement.RemoveElementMethod(nums, val);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/RemoveElement.cs
@@ -0,0 +1,25 @@
+﻿namespace InterviewbitAndLitcode
+{
+    //https://leetcode.com/problems/remove-element
+    public class RemoveElement
+    {
+        public int RemoveElementMethod(int[] nums, int val)
+        {
+            var tempArray = new int[nums.Length];
+            int j = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    tempArray[j] = nums[i];
+                    j++;
+                }
+            }
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                nums[i] = tempArray[i];
+            }
+            return j;
+        }
+    }
+}
