664333924cc8b831343721339ad8ac84941a3d04
Ernan <maisels@gmail.com>
2024-03-13 11:09:54 +0200

    improve solution

--- InterviewbitAndLitcode/MergeSortedArray.cs
+++ InterviewbitAndLitcode/MergeSortedArray.cs
@@ -1,33 +1,28 @@
 ﻿namespace InterviewbitAndLitcode
 {
+    //https://leetcode.com/problems/merge-sorted-array/submissions/1202374046/
     public class MergeSortedArray
     {
         public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
-            int i = m - 1; 
-            int j = n - 1; 
-            int k = m + n - 1; 
 
-            while (i >= 0 && j >= 0)
+            var nums1Index = m - 1;
+            var nums2Index = n - 1;
+            var mergedIndex = m + n - 1;
+
+            while (nums2Index >= 0)
             {
-                if (nums1[i] > nums2[j])
+                if (nums1Index >= 0 && nums1[nums1Index] > nums2[nums2Index])
                 {
-                    nums1[k] = nums1[i];
-                    i--;
+                    nums1[mergedIndex] = nums1[nums1Index];
+                    nums1Index--;
                 }
                 else
                 {
-                    nums1[k] = nums2[j];
-                    j--;
+                    nums1[mergedIndex] = nums2[nums2Index];
+                    nums2Index--;
                 }
-                k--;
-            }
-
-            while (j >= 0)
-            {
-                nums1[k] = nums2[j];
-                j--;
-                k--;
+                mergedIndex--;
             }
         }
     }
