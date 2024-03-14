79f9cf5f7f59cd957cb1945e6fde7c9ef315855a
Ernan <maisels@gmail.com>
2024-03-14 07:24:19 +0200

    RemoveDuplicatesFromSortedArray

---
+++ InterviewbitAndLitcode.Tests/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,23 @@
+﻿namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+        [TestMethod()]
+        public void RemoveDuplicatesTest()
+        {
+            // Arrange
+            var removeDuplicatesFromSortedArray = new RemoveDuplicatesFromSortedArray();
+            var nums = new int[] { 1, 1, 2 };
+
+            // Act
+            var result = removeDuplicatesFromSortedArray.RemoveDuplicates(nums);
+
+            // Assert
+            Assert.AreEqual(2, result);
+            
+
+
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,23 @@
+﻿namespace InterviewbitAndLitcode
+{
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array
+    public class RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var tempList = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!tempList.Any(x => x == nums[i]))
+                {
+                    tempList.Add(nums[i]);
+                }
+            }
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                nums[i] = tempList[i];
+            }
+            return tempList.Count;
+        }
+    }
+}
