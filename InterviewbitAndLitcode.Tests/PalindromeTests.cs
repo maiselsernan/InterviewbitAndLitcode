88d6553ba372bc0ba24df1c025b720f62e59d1c5
Ernan <maisels@gmail.com>
2024-03-16 23:23:14 +0200

    Palindrome

---
+++ InterviewbitAndLitcode.Tests/PalindromeTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewbitAndLitcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class PalindromeTests
+    {
+        [TestMethod()]
+        public void IsPalindromeTest()
+        {
+            //arrange
+            var palindrome = new Palindrome();
+            int x = 121;
+            bool expected = true;
+            //act
+            bool actual = palindrome.IsPalindrome(x);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void IsNotPalindromeTest()
+        {
+            //arrange
+            var palindrome = new Palindrome();
+            int x = 123;
+            bool expected = false;
+            //act
+            bool actual = palindrome.IsPalindrome(x);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/Palindrome.cs
@@ -0,0 +1,19 @@
+﻿namespace InterviewbitAndLitcode
+{
+    //https://leetcode.com/problems/palindrome-number/description/
+    public class Palindrome
+    {
+        public bool IsPalindrome(int x)
+        {
+            var str = x.ToString();
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
