3d484944774e32b61af96ba19982bc7a031efd62
Ernan <maisels@gmail.com>
2024-03-17 12:05:55 +0200

    RemoveDuplicates

--- InterviewbitAndLitcode/RemoveDuplicatesFromSortedArray2.cs
+++ InterviewbitAndLitcode/RemoveDuplicatesFromSortedArray2.cs
@@ -5,32 +5,42 @@
     {
         public int RemoveDuplicates(int[] nums)
         {
-            if (nums.Length == 0)
+            //if (nums.Length == 0)
+            //{
+            //    return 0;
+            //}
+            //int i = 0;
+            //int count = 1;
+            //for (int j = 1; j < nums.Length; j++)
+            //{
+            //    if (nums[j] == nums[i])
+            //    {
+            //        count++;
+            //        if (count <= 2)
+            //        {
+            //            i++;
+            //            nums[i] = nums[j];
+            //        }
+            //    }
+            //    else
+            //    {
+            //        i++;
+            //        nums[i] = nums[j];
+            //        count = 1;
+            //    }
+            //}
+            //return i + 1;
+            var replaceIndex = 0;
+            for (var i = 0; i < nums.Length; i++)
             {
-                return 0;
-            }
-            int i = 0;
-            int count = 1;
-            for (int j = 1; j < nums.Length; j++)
-            {
-                if (nums[j] == nums[i])
+                if (replaceIndex - 2 >= 0 && nums[replaceIndex - 2] == nums[i])
                 {
-                    count++;
-                    if (count <= 2)
-                    {
-                        i++;
-                        nums[i] = nums[j];
-                    }
+                    continue;
                 }
-                else
-                {
-                    i++;
-                    nums[i] = nums[j];
-                    count = 1;
-                }
+                nums[replaceIndex] = nums[i];
+                replaceIndex++;
             }
-            return i + 1;
-
+            return replaceIndex;
         }
     }
 }
