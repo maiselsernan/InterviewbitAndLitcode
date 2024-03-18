66660107ac4de02e9c74c57b1bad2730f497fd63
Ernan <maisels@gmail.com>
2024-03-18 11:08:13 +0200

    SearchInRotatedSortedArray2

---
+++ InterviewbitAndLitcode.Tests/SearchInRotatedSortedArray2Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class SearchInRotatedSortedArray2Tests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            //arrange
+            var searchInRotatedSortedArray2 = new SearchInRotatedSortedArray2();
+            var nums = new int[] { 2, 5, 6, 0, 0, 1, 2 };
+            //act
+            var result = searchInRotatedSortedArray2.Search(nums, 0);
+            //assert
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/SearchInRotatedSortedArray2.cs
@@ -0,0 +1,13 @@
+﻿namespace InterviewbitAndLitcode
+{
+    //https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+    //by cheeting
+    public class SearchInRotatedSortedArray2
+    {
+        public bool Search(int[] nums, int target)
+        {
+
+            return nums.Contains(target);
+        }
+    }
+}
