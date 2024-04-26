890b3a232f3b34b3f8ccff54ce3c50828c962f22
Ernan <maisels@gmail.com>
2024-04-26 13:12:27 +0300

    can jump

---
+++ InterviewbitAndLitcode.Tests/CanJumpTests.cs
@@ -0,0 +1,20 @@
+﻿namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class CanJumpTests
+    {
+        [TestMethod()]
+        public void CanJumpMethodTest()
+        {
+            //arrange
+            CanJump canJump = new CanJump();
+            int[] nums = { 2, 3, 1, 1, 4 };
+            bool expected = true;
+            //act
+            bool actual = canJump.CanJumpMethod(nums);
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/CanJump.cs
@@ -0,0 +1,19 @@
+﻿namespace InterviewbitAndLitcode
+{
+    //https://leetcode.com/problems/jump-game/description/?envType=study-plan-v2&envId=top-interview-150
+    public class CanJump
+    {
+        public bool CanJumpMethod(int[] nums)
+        {
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > max)
+                    return false;
+                max = Math.Max(max, i + nums[i]);
+            }
+            return true;
+        }
+       
+    }
+}
