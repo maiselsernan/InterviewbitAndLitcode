0229b81d90f434447fa62ff02067e71a9d4ec20b
Ernan <maisels@gmail.com>
2024-04-26 13:12:39 +0300

    binary search

---
+++ InterviewbitAndLitcode.Tests/BinarySearchTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewbitAndLitcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class BinarySearchTests
+    {
+        [TestMethod()]
+        public void BinarySearchMethodTest()
+        {
+            //arrange
+            BinarySearch binarySearch = new BinarySearch();
+            int[] nums = { 1, 3, 5, 6 };
+            int target = 5;
+            //act
+            int actual = binarySearch.BinarySearchMethod(nums, target);
+            //assert
+            Assert.AreEqual(2, actual);
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/BinarySearch.cs
@@ -0,0 +1,22 @@
+﻿namespace InterviewbitAndLitcode
+{
+    public class BinarySearch
+    {
+        public int BinarySearchMethod(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                if (nums[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return -1;
+        }
+    }
+}
