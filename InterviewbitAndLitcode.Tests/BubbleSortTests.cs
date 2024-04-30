b856688376e8934e55a20a4b3564ffa35b82c7f4
Ernan <maisels@gmail.com>
2024-04-30 14:38:29 +0300

    BubbleSort

---
+++ InterviewbitAndLitcode.Tests/BubbleSortTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewbitAndLitcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class BubbleSortTests
+    {
+        [TestMethod()]
+        public void BubbleSortMethodTest()
+        {
+            //arrange
+            BubbleSort bubbleSort = new BubbleSort();
+            int[] nums = new int[] { 64, 34, 25, 12, 22, 11, 90 };
+            int[] expected = new int[] { 11, 12, 22, 25, 34, 64, 90 };
+
+            //act
+            bubbleSort.BubbleSortMethod(nums);
+
+            //assert
+            CollectionAssert.AreEqual(expected, nums);
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode
+{
+    public class BubbleSort
+    {
+        public void BubbleSortMethod(int[] nums)
+        {
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n -1 - i; j++)
+                {
+                    if (nums[j] > nums[j+1])
+                    {
+                        var temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
