716d94d8ca658a131995e09bde78d696a119ccb3
Ernan <maisels@gmail.com>
2024-05-07 09:53:36 +0300

    RotateArray

---
+++ InterviewbitAndLitcode.Tests/RotateArrayTests.cs
@@ -0,0 +1,26 @@
+﻿namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class RotateArrayTests
+    {
+        [TestMethod()]
+        public void RotateArrayMethodTest()
+        {
+            //arrange
+            //int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            int[] nums = new int[] { -1, -100, 3, 99 };
+            //int[] nums = new int[] { 1, 2 };
+            //int k = 3;
+            int k = 2;
+            //int[] expected = new int[] { 5, 6, 7, 1, 2, 3, 4 };
+            int[] expected = new int[] { 3, 99, -1, -100 };
+            //int[] expected = new int[] {2,1 };
+            RotateArray rotateArray = new RotateArray();
+            //act
+            rotateArray.RotateArrayMethod(nums, k);
+            //assert
+            CollectionAssert.AreEqual(expected, nums);
+
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/RotateArray.cs
@@ -0,0 +1,19 @@
+﻿namespace InterviewbitAndLitcode
+{
+    public class RotateArray
+    {
+        public void RotateArrayMethod(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            int[] temp = new int[nums.Length];
+
+            Array.Copy(nums, nums.Length - k, temp, 0, k);
+
+            Array.Copy(nums, 0, temp, k, nums.Length - k);
+
+            Array.Copy(temp, nums, nums.Length);
+        }
+
+       
+    }
+}
