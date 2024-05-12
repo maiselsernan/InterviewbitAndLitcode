0b6e0f267bcf50bba52911e3755eeb21690bb8ee
Ernan <maisels@gmail.com>
2024-05-12 10:09:45 +0300

    max profit

---
+++ InterviewbitAndLitcode.Tests/MaxProfitTests.cs
@@ -0,0 +1,21 @@
+﻿namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class MaxProfitTests
+    {
+        [TestMethod()]
+        public void maxProfitTest()
+        {
+            //arrange
+            MaxProfit maxProfit = new MaxProfit();
+            int[] prices = new int[] { 3, 2, 6, 5, 0, 3 };
+            //int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+            int expected = 4;
+            //int expected = 5;
+            //act
+            int actual = maxProfit.maxProfit(prices);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/MaxProfit.cs
@@ -0,0 +1,29 @@
+﻿namespace InterviewbitAndLitcode
+{
+    public class MaxProfit
+    {
+        //7, 1, 5, 3, 6, 4
+        //3,2,6,5,0,3
+        public int maxProfit(int[] prices)
+        {
+            if (prices.Length == 0)
+                return 0;
+            int minPrice = prices[0];
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                    continue;
+                }
+                if ((prices[i] - minPrice) > maxProfit)
+                {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+            return maxProfit;
+        }
+    }
+}
