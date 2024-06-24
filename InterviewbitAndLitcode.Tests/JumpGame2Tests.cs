231df37139cb6d52c8d48766100525917f11c35f
Ernan <maisels@gmail.com>
2024-06-24 17:10:00 +0300

    JumpGame2

---
+++ InterviewbitAndLitcode.Tests/JumpGame2Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewbitAndLitcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class JumpGame2Tests
+    {
+        [TestMethod()]
+        public void JumpTest()
+        {
+            //arrange
+            int[] nums = new int[] { 2, 3, 1, 1, 4 };
+            JumpGame2 jumpGame2 = new JumpGame2();
+            int expected = 2;
+            //act
+            int actual = jumpGame2.Jump(nums);
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/JumpGame2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode
+{
+    public class JumpGame2
+    {
+        public int Jump(int[] nums)
+        {
+            int n = nums.Length;
+            if (n == 1)
+                return 0;
+            int[] dp = new int[n];
+            dp[0] = 0;
+            for (int i = 1; i < n; i++)
+            {
+                dp[i] = int.MaxValue;
+                for (int j = 0; j < i; j++)
+                {
+                    if (j + nums[j] >= i)
+                    {
+                        dp[i] = Math.Min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+            return dp[n - 1];
+        }
+    }
+}
