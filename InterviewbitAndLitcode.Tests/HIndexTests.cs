be6f73400f744c2706ba8cf3242641083910c825
Ernan <maisels@gmail.com>
2024-06-25 14:22:31 +0300

    HIndex

---
+++ InterviewbitAndLitcode.Tests/HIndexTests.cs
@@ -0,0 +1,19 @@
+﻿namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class HIndexTests
+    {
+        [TestMethod()]
+        public void HIndexMethodTest()
+        {
+            //arrange
+            HIndex hIndex = new HIndex();
+            int[] citations = new int[] { 3, 0, 6, 1, 5 };
+            int expected = 3;
+            //act
+            int actual = hIndex.HIndexMethod(citations);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/HIndex.cs
@@ -0,0 +1,23 @@
+﻿namespace InterviewbitAndLitcode
+{
+    public class HIndex
+    {
+        public int HIndexMethod(int[] citations)
+        {
+            citations = citations.OrderByDescending(x => x).ToArray();
+            int h = 0;
+            for (int i = 0; i < citations.Length; i++)
+            {
+                if (citations[i] >= i + 1)
+                {
+                    h = i + 1;
+                    continue;
+                }
+                break;
+            }
+            return h;
+        }
+    }
+}
+
+
