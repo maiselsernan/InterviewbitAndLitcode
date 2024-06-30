2409253cce760fb5c0bae1d61fb24f5d5c690842
Ernan <maisels@gmail.com>
2024-06-30 15:23:18 +0300

    needle in a hystack

---
+++ InterviewbitAndLitcode.Tests/IndexOfFirstAccurrenceTests.cs
@@ -0,0 +1,20 @@
+﻿namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class IndexOfFirstAccurrenceTests
+    {
+        [TestMethod()]
+        public void StrStrTest()
+        {
+            //arrange
+            IndexOfFirstAccurrence test = new IndexOfFirstAccurrence();
+            string haystack = "mississippi";
+            string needle = "pi";
+            int expected = 9;
+            //act
+            int actual = test.StrStr(haystack, needle);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/IndexOfFirstAccurrence.cs
@@ -0,0 +1,35 @@
+﻿namespace InterviewbitAndLitcode
+{
+    public class IndexOfFirstAccurrence
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0)
+                return 0;
+            if (haystack.Length < needle.Length)
+                return -1;
+
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if (haystack[i] == needle[0] && Compare(haystack, i, needle))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private bool Compare(string haystack, int index, string needle)
+        {
+            for (int j = 0; j < needle.Length; j++)
+            {
+                if (haystack[index + j] != needle[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
