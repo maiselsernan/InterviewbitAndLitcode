09b483ce6bd288e2ce5c5842e82f817d52bfd5b6
Ernan <maisels@gmail.com>
2024-07-11 13:01:52 +0300

    IsPalindrome

---
+++ InterviewbitAndLitcode.Tests/ValidPalindromeTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewbitAndLitcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class ValidPalindromeTests
+    {
+        [TestMethod()]
+        public void IsPalindromeTest()
+        {
+            //arrange
+            ValidPalindrome test = new ValidPalindrome();
+            //act
+            bool result = test.IsPalindrome("0P");
+            //bool result = test.IsPalindrome("A man, a plan, a canal: Panama");
+            //assert
+            Assert.IsFalse(result);
+
+        }
+    }
+}
---
+++ InterviewbitAndLitcode/ValidPalindrome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewbitAndLitcode
+{
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                while (left < right && !char.IsLetterOrDigit(s[left]))
+                {
+                    left++;
+                }
+
+                while (left < right && !char.IsLetterOrDigit(s[right]))
+                {
+                    right--;
+                }
+                if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+         
+        }
+    }
+}
