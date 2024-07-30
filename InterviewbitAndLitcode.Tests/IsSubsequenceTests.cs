9098933934d9531476620adb529f8a8b48dfb8fb
Ernan <maisels@gmail.com>
2024-07-30 11:03:32 +0300

    IsSubsequence

---
+++ InterviewbitAndLitcode.Tests/IsSubsequenceTests.cs
@@ -0,0 +1,35 @@
+﻿namespace InterviewbitAndLitcode.Tests
+{
+    [TestClass()]
+    public class IsSubsequenceTests
+    {
+        [TestMethod()]
+        public void IsSubsequenceMethodTestShouldSucceed()
+        {
+
+            //Arrange
+            var isSubsequence = new IsSubsequence();
+            var s = "abc";
+            var t = "ahbgdc";
+            var expected = true;
+            //Act
+            var actual = isSubsequence.IsSubsequenceMethod(s, t);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void IsSubsequenceMethodTestShouldFail()
+        {
+
+            //Arrange
+            var isSubsequence = new IsSubsequence();
+            var s = "axc";
+            var t = "ahbgdc";
+            var expected = false;
+            //Act
+            var actual = isSubsequence.IsSubsequenceMethod(s, t);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        }
+}
---
+++ InterviewbitAndLitcode/IsSubsequence.cs
@@ -0,0 +1,28 @@
+﻿namespace InterviewbitAndLitcode
+{
+    public class IsSubsequence
+    {
+        public bool IsSubsequenceMethod(string s, string t)
+        {
+            if (s.Length == 0)
+            {
+                return true;
+            }
+            int i = 0;
+            int j = 0;
+            while (j < t.Length)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                    if (i == s.Length)
+                    {
+                        return true;
+                    }
+                }
+                j++;
+            }
+            return false;
+        }
+    }
+}
